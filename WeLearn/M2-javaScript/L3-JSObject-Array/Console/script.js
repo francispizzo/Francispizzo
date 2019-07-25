acde2d5eeb15dd55bd76547c54da821b40bcba0c
Francis Pizzo <pizzo004@csusm.edu>
2019-07-25 12:07:37 -0700

    more buttons

---
+++ WeLearn/M2-javaScript/L3-JSObject-Array/Console/script.js
@@ -0,0 +1,51 @@
+const article = {
+  name: 'Dogfamily gives birth to 10 puppies' ,
+  views: 1035,
+  datepublished: '03/25/2018',
+  author: {
+    name: 'Joe Corgi',
+    title: 'Senior Canine Editor',
+  }
+};
+
+
+const classroom = {
+  name: 'CSSI' ,
+  students: 29 ,
+  hopes: 'questionable?' ,
+  dreams: 'crushed' ,
+};
+
+const food = {
+  fruit: {
+    strawberries: 'sliced' ,
+    pineapples: 'diced' ,
+    apples: 'whole' ,
+  },
+  meat: {
+    hamburger: 'cheese' ,
+    sausage: 'brautwurst' ,
+    coldcuts: 'salami' ,
+  },
+  italian: {
+    mamamia: 'spaghetti' ,
+    mamamamamia: 'chicken parm' ,
+    mamamamamamamia: 'lasaganana' ,
+  },
+  dessert: {
+    icecream: 'chcocococ' ,
+  }
+};
+const seasons = ['spring','summer','fall','winter'];
+const friends = ['Alice', 'Bob', 'Charlie'];
+const numbers = [5, 10, 2, 16, 50];
+
+const classes = [];
+ classes.push({course: 'CS 111', title: 'Computer science I'});
+ classes.push({course: 'Math 160', title: 'Calculus I'});
+
+ const party = {
+   date: '7/4/2018',
+   location: 'Library',
+   guestS: ['Mary', 'Joe', 'Smith']
+ };
