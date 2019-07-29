7ed4016e888b7f5b727a19ac780382c82f7cd43a
Francis Pizzo <pizzo004@csusm.edu>
2019-07-29 14:24:43 -0700

    sosig

--- WeLearn/M3-Python/labs/Lav1-Pluralizeit/pluralize.py
+++ WeLearn/M3-Python/labs/Lav1-Pluralizeit/pluralize.py
@@ -17,3 +17,28 @@ if number == 1:
     print ((str(number))+(" ")+(the_word))
 else:
     print ((str(number))+(" ")+pluralize(the_word))
+
+
+
+import webapp2
+import jinja2
+import os
+
+
+the_jinja_env = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
+
+
+# the handler section
+class EnterInfoHandler(webapp2.RequestHandler):
+    def get(self):  # for a get request
+        welcome_template = the_jinja_env.get_template('templates/welcome.html')
+        a_variable_dict = {
+            "greeting": "Salutations",
+            "adjective": "dank"
+        }
+        self.response.write(welcome_template.render(a_variable_dict))
+
+], debug=True)
